306555459c6b7a782da6d4335fd53599cbdde577
maschzh <maschzh_001@163.com>
2023-07-15 06:05:19 +0800

    set dioxus project

---
+++ front/src/components/mod.rs
@@ -0,0 +1,11 @@
+mod footer;
+mod header;
+mod button;
+mod film_card;
+mod film_modal;
+
+pub use footer::Footer;
+pub use header::Header;
+pub use button::Button;
+pub use film_card::FilmCard;
+pub use film_modal::FilmModal;
